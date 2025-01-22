5efbc66aa8d3dbea2137e99a7a19ae18e7ec57fc
liu <523420504@qq.com>
2025-01-22 23:30:16 +0800

    HashMap1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-fn main() {
-    //字符串的切割
-    let s = "我是Jisoo";
-    //4不是字符的边界,这里切割会panic
-    // let split = &s[..4];
-    // println!("{}",split);
-    let sp = &s[..6];
-    println!("{}",sp);
+use std::collections::HashMap;
 
-    //遍历String, 字节bytes() 标量值chars()
+fn main() {
+    /*
+        HashMap<K,V>
+        insert()
+        数据都存在heap中
+     */
+    let mut scores:HashMap<i32, String> = HashMap::new();
+
+    scores.insert(1, "Jisoo".to_string());
+    scores.insert(2, "Rose".to_string());
+
+    //使用collect方法
+    let teams = vec![String::from("LA"),String::from("CHA")];
+    let initial_scores = vec![10,50];
+
+    let team2scores:HashMap<_,_> = 
+        teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}",team2scores);
 }
