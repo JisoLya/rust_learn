c452febe1bcae5f50af6ccab3422545ee81378d1
liu <523420504@qq.com>
2025-01-22 23:13:41 +0800

    string-format拼接

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 fn main() {
-    let s1 = String::from("Hello ,");
-    let s2 = String::from("world");
+    // 使用format拼接字符串
+    let s1 = String::from("aaa");
+    let s2 = String::from("bbb");
+    let s3 = String::from("ccc");
 
-    //这里+相当于调用了一个方法
-    /*
-        fn add(self, s:&str) -> String{...}
-        只能把&str添加到String类型上
-        这里&s2是String的引用类型，rust使用了解引用强制转换的方法
-
-        而且由于第二个参数是借用类型，第二个参数的所有权会保留，第一个参数没有&符号，函数会获得s1的所有权
-        因此打印会报错
-     */
-    let s3 = s1 + &s2;
-
-    // println!("{}",s1);
-    println!("{}",s2);
-    println!("{}",s3);
+    let s = format!("{}-{}-{}",s1,s2,s3);
+    println!("{}",s);
 }
