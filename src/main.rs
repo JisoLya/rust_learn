ae1df556456e8d0d37682fd4fb8ee07416c93950
liu <523420504@qq.com>
2025-01-23 00:20:20 +0800

    HashMap3

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,38 @@
 use std::collections::HashMap;
 
 fn main() {
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
-
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
-
-    //对于实现了copy trait的类型，值会被复制到HashMap中，而所有权不会改变，
-    //对于拥有所有权的值，值会被移动，所有权会转移到HashMap中
-    //如果把值的引用存入到HashMap中，那么所有权不会改变
-
-    // println!("{} : {}",field_name,field_value);
-
-
+    //修改HashMap,直接覆盖
     let mut scores = HashMap::new();
-    let blue = String::from("Blue");
-    let yellow = String::from("Yellow");
-
-    scores.insert(&blue, 10);
-    scores.insert(&yellow, 50);
+    scores.insert(String::from("Blue"), 1);
+    scores.insert(String::from("Blue"), 10);
+    println!("{:?}",scores);
 
     /*
-        访问HashMap中的值
-        get(key)方法
-        返回Option<&V>
+     * 先检查一个key，如果不存在才插入
+     * entry()方法,
+     * 返回：enum Entry，代表值是否存在
+     * 
+     * or_insert()方法
+     * - 返回：
+     * 如果key存在，返回到对应v的一个可变引用
+     * 如果key不存在，将方法参数作为k的新值插进去，返回这个值的可变引用
+     * 
      */
-    match scores.get(&blue){
-        Some(s) => println!("{}",s),
-        None => println!("Team does not exist")
-    }
+    scores.entry(String::from("Blue")).or_insert(123);
+    scores.entry(String::from("Yellow")).or_insert(123);
+    println!("{:?}",scores);
+
 
     /*
-        遍历HashMap
+     *  基于现有的V来更新V
+     *  
      */
-    for (k, v) in scores{
-        println!("key = {}, value = {}",k,v);
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_ascii_whitespace(){
+        let count = map.entry(word).or_insert(0);
+        *count+=1;
     }
+    println!("{:?}",map);
 }
