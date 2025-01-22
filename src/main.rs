3199478569c4c9c92e8c6081a6a6f0d6335e415e
liu <523420504@qq.com>
2025-01-22 23:41:09 +0800

    HashMap的访问与获取元素

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
 use std::collections::HashMap;
 
 fn main() {
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    //对于实现了copy trait的类型，值会被复制到HashMap中，而所有权不会改变，
+    //对于拥有所有权的值，值会被移动，所有权会转移到HashMap中
+    //如果把值的引用存入到HashMap中，那么所有权不会改变
+
+    // println!("{} : {}",field_name,field_value);
+
+
+    let mut scores = HashMap::new();
+    let blue = String::from("Blue");
+    let yellow = String::from("Yellow");
+
+    scores.insert(&blue, 10);
+    scores.insert(&yellow, 50);
+
     /*
-        HashMap<K,V>
-        insert()
-        数据都存在heap中
+        访问HashMap中的值
+        get(key)方法
+        返回Option<&V>
      */
-    let mut scores:HashMap<i32, String> = HashMap::new();
+    match scores.get(&blue){
+        Some(s) => println!("{}",s),
+        None => println!("Team does not exist")
+    }
 
-    scores.insert(1, "Jisoo".to_string());
-    scores.insert(2, "Rose".to_string());
-
-    //使用collect方法
-    let teams = vec![String::from("LA"),String::from("CHA")];
-    let initial_scores = vec![10,50];
-
-    let team2scores:HashMap<_,_> = 
-        teams.iter().zip(initial_scores.iter()).collect();
-    println!("{:?}",team2scores);
+    /*
+        遍历HashMap
+     */
+    for (k, v) in scores{
+        println!("key = {}, value = {}",k,v);
+    }
 }
