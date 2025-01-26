67cc84cc97cf077d7ed8ca3d9743e503dc5d9176
liu <523420504@qq.com>
2025-01-26 22:22:30 +0800

    错误处理1

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,32 @@
-use std::collections::HashMap;
-
+use std::fs::File;
 fn main() {
-    //修改HashMap,直接覆盖
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 1);
-    scores.insert(String::from("Blue"), 10);
-    println!("{:?}",scores);
+    /*
+        Rust的错误处理机制
+        不可恢复的错误与panic!宏
+        当执行panic!时：
+        - 打印一个错误信息
+        - 展开(unwind) 清理调用栈
+        - 退出程序    
+     */
+    // panic!("crash and burn!");
 
     /*
-     * 先检查一个key，如果不存在才插入
-     * entry()方法,
-     * 返回：enum Entry，代表值是否存在
-     * 
-     * or_insert()方法
-     * - 返回：
-     * 如果key存在，返回到对应v的一个可变引用
-     * 如果key不存在，将方法参数作为k的新值插进去，返回这个值的可变引用
-     * 
+        可恢复错误与Result枚举
+        enum Result<T,E>{
+            Ok(T),
+            Err(E),
+        }
+        T:操作成功情况下，Ok变体里返回的数据类型
+        E:操作失败的情况下，Err变体里返回的错误类型
      */
-    scores.entry(String::from("Blue")).or_insert(123);
-    scores.entry(String::from("Yellow")).or_insert(123);
-    println!("{:?}",scores);
-
-
-    /*
-     *  基于现有的V来更新V
-     *  
-     */
-
-    let text = "hello world wonderful world";
-    let mut map = HashMap::new();
-    for word in text.split_ascii_whitespace(){
-        let count = map.entry(word).or_insert(0);
-        *count+=1;
+    let f = File::open("hello.txt");
+    match f{
+        Ok(file) => {println!("open success:{:?}",file)},
+        Err(data) =>{println!("open fail!err:{}",data)}
     }
-    println!("{:?}",map);
+    /*
+        unwrap 以及 expect
+     */
+    
+
 }
