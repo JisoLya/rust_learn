619ea19c439f47a7550b2e0115e59cef1ef9d48e
liu <523420504@qq.com>
2025-01-26 22:32:51 +0800

    match匹配错误类型

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::fs::File;
+use std::io::ErrorKind;
 fn main() {
     let f = File::open("hello.txt");
 
+    //如何匹配不同的错误呢?
     let f = match f {
         Ok(file) => file,
-        Err(err) => {
-            panic!("Error opening file {:?}",err);
-        }
+        Err(err) => match err.kind(){
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Error creating file: {:?}",e),
+            },
+            other_error => panic!("Error opening file: {:?}",other_error),
+        },
     };
-    //如何匹配不同的错误呢?
     
 }
