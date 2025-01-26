2bd06cb24116c5bc2559547cbe3d91dddefe7ffb
liu <523420504@qq.com>
2025-01-26 22:35:51 +0800

    unwrap方法

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::fs::File;
-use std::io::ErrorKind;
 fn main() {
-    let f = File::open("hello.txt");
+    // let f = File::open("hello.txt");
 
-    //如何匹配不同的错误呢?
-    let f = match f {
-        Ok(file) => file,
-        Err(err) => match err.kind(){
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Error creating file: {:?}",e),
-            },
-            other_error => panic!("Error opening file: {:?}",other_error),
-        },
-    };
+    // //如何匹配不同的错误呢?
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(err) => {
+    //         panic!("Error opening file:{:?}",err)
+    //     }
+    // };
     
-}
+    /*
+        unwrap方法,相当于上边的语句
+
+     */
+    let f = File::open("hello.txt").unwrap();
+}   
