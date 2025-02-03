bdac24e51a6e3d5fd8783686672eba0065e355c9
liu <523420504@qq.com>
2025-02-04 00:46:10 +0800

    编写测试3 其他的一些命令

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
 fn main() {
     /*
-    验证错误：should_panic属性
-    测试出现panic会测试通过
+    cargo test默认行为:
+    - 并行运行
+    - 所有测试
+    - 不捕获所有的输出 (println!()这样的)
+
+    显示test之后的可用参数
+    cargo test --help
+    显示--之后可选的参数
+    cargo test -- --help
+
+    控制线程数量：
+        cargo test -- --test-threads=1
+    显示函数输出:
+        cargo test -- --show-output
+    按名称运行测试
+        cargo test xxx测试名称(会自动进行匹配)
+
+    加入#[ignore]的测试不会被运行
+    运行被忽略的测试
+    cargo test -- --ignored
      */
 }
 
 #[test]
-//可以指定错误信息，当不是assertion failed 时测试不会通过
-#[should_panic(expected = "assertion failed")]
-fn test(){
-    panic!("111");
-    // panic!("assertion failed");
+fn test() {
+    println!("Hello, world!");
 }
+
+#[test]
+fn test2_() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn test_3() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn test_42() {}
