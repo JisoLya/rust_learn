952b88853e7e77312c2dd77666e7f8fbff77f3ca
liu <523420504@qq.com>
2025-02-05 23:32:38 +0800

    读取环境变量实现大小写敏感,或者通过命令行cargo run xx xxx --case_insensitive|case_sensitive

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-use my_project::Config;
-use std::{env, process};
-
 fn main() {
-    /*
-        项目：
-        简单版本的grep
-     */
-    let args: Vec<String> = env::args().collect();
-    // println!("{:?}",args);
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-
-    if let Err(e) = my_project::run(config){
-        eprintln!("Application error: {}", e);
-        process::exit(1);
-    };
-    //unwrap_or_else 调用一个闭包
+    
 }
