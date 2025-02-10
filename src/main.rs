c21599a0553f60642d0e28bf2d1fcaa573a4f204
liu <523420504@qq.com>
2025-02-11 00:51:01 +0800

    自定义profile

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,14 @@
 fn main() {
     /*
-    使用Iterator trait实现自定义迭代器
+    release profile
+    - 预定义的
+    - 可自定义：可以使用不同的配置，对代码编译拥有更多的控制
+    
+    Cargo主要的两个profile
+    - dev profile 执行cargo build
+    - release profile适用于发布，cargo build --release
+    
+    自定义profile
+    在cargo.toml添加[profile.xxx]区域，覆盖默认的子集即可，看Cargo.toml...
      */
-    let mut counter = Counter::new();
-    assert_eq!(counter.next(), Some(1));
-    assert_eq!(counter.next(), Some(2));
-    assert_eq!(counter.next(), Some(3));
-    assert_eq!(counter.next(), Some(4));
-    assert_eq!(counter.next(), Some(5));
-    assert_eq!(counter.next(), None);
-}
-
-struct Counter{
-    count: u32,
-}
-
-impl Counter {
-    fn new() -> Counter {
-        Counter{count: 0}
-    }
-}
-
-impl Iterator for Counter{
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item>{
-        if self.count < 5{
-            self.count += 1;
-            Some(self.count)
-        }else{
-            None
-        }
-    }
 }
