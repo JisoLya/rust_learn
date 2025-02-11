124e10b705b2067a4cc299f61ed0677954e84a4c
liu <523420504@qq.com>
2025-02-12 00:49:02 +0800

    Deref Trait

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,65 @@
+use std::ops::Deref;
+
 fn main() {
     /*
-    智能指针是一种数据结构：
-    - 行为和指针类似
-    - 拥有额外的元数据和额外的功能
-    
-    引用计数(reference counting)智能指针类型
-    - 通过记录所有者的数量，使一份数据被多个所有者同时拥有，并且在没有使用者时清理掉数据
-    智能指针的例子：
-    String Vec<T>
-    智能指针的实现:
-    一般用struct实现，并且实现了Deref 和 Drop两个trait
-    
-    Deref trait 允许智能指针struct的实例像引用一样使用
-    Drop trait 允许自定义智能指针离开作用域时执行的代码
+    Deref Trait
+    - 实现deref trait使我们可以自定义解引用运算符*的行为
+    - 通过实现Deref，智能指针可以通过常规的方法来解引用
      */
+    let x = 5;
+    let y = &x;
+    let z = Box::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5,*y);
+    //报错
+    // assert_eq!(5, y);
+    assert_eq!(5, *z);
+    
+    
+    //
+    let y = MyBox::new(5);
+    //为了支持解引用方法，需要实现Deref trait
+    assert_eq!(5,*y);
+    /*
+    函数和方法的隐式解引用转化，假设T实现了Deref Trait
+    当某个类型的引用传递给方法或者函数的时候，他的类型与定义的参数类型不匹配，Deref Coercion就会自动发生，
+    编译器会经过一系列调用，把他转化为所需的参数类型
+     */
+    let m = MyBox::new(String::from("Rust"));
+    /* 
+    &m &MyBox<String>
+           |
+     deref &String
+           |
+       deref &str
+    */ 
+    hello(&m);
+    // 下边的a 相当于一系列的deref
+    // let a = &(*m)[..];
+    hello("Rust");
     
     /*
-    Box<T>
-    常用场景:
-        - 在编译时，某类型的大小无法确认，而使用该类型时，上下文却需要知道他的确切大小
-        - 当有大量数据，想移交所有权，但需确保在操作时不会被复制
-        - 某个值你只关心他是否实现了特定的trait而不关心他的具体类型
-        
+    解引用与可变性
+    当类型和trait在下列三种情况发生时，Rust会执行deref coercion：
+    - 当T:Deref<Target=U>，允许 &T转换为&U
+    - 当T:DerefMut<Target=U>,允许&mut T转换为&mut U
+    - 当T:Deref<Target=U>,允许&mut T 转换为&U
      */
-    let b = Box::new(5);
-    println!("{}", b);
 }
-enum List{
-    Cons(i32, Box<List>),
-    Nil,
+
+struct MyBox<T>(T);
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
