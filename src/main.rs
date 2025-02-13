ea0c2ba6af1d9ecf6fbe7f1a6fd3c75734ad44ec
liuyan <SoLiuyan@163.com>
2025-02-13 22:41:25 +0800

    Arc<T>实现原子计数

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
     /*
@@ -7,10 +8,27 @@ fn main() {
     
     访问数据前，通过lock方法来获取锁，会阻塞当前线程，lock可能会失败，返回一个MutexGuard(智能指针，实现了Deref和Drop)
      */
-    let m = Mutex::new(5);
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    
+    for _ in 1..10 {
+        //这里会移交所有权,而Rc只适合单线程
+        //可以使用Arc<T>来进行原子计数
+        // let handle = thread::spawn(move || {
+        //    let mut num =  counter.lock().unwrap();
+        //     *num += 1;
+        // });
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move||{
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
     }
-    println!("m = {:?}", m);
+    
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    
+    println!("Result: {}", *counter.lock().unwrap());
 }
