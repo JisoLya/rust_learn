70de56214bbb4333935305ad1ce0d8a838a93303
liuyan <SoLiuyan@163.com>
2025-02-13 22:09:49 +0800

    线程1

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,26 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-use crate::List::{Cons, Nil};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     /*
-        循环引用导致内存泄露
+    rust 多线程,通过thread::spawn创建一个线程，接受一个闭包作为参数
+    
+    并且可以使用join.Handle等待所有线程的完成
+    
+    使用move闭包可以把一个线程的所有权转移到另一个线程当中
      */
-    let a = Rc::new(Cons(5,RefCell::new(Rc::new(Nil))));
     
-    println!("a initial rc count = {}", Rc::strong_count(&a));
-    println!("a next item = {:?}", a.tail());
-    
-    let b = Rc::new(Cons(10,RefCell::new(Rc::clone(&a))));
-    println!("a rc count after b creation = {}", Rc::strong_count(&a));
-    println!("b initial rc count = {}", Rc::strong_count(&b));
-    println!("b next item = {:?}", b.tail());
-    
-    if let Some(link) = a.tail() {
-        *link.borrow_mut() = Rc::clone(&b);
-    }
-    println!("b rc count after changing a = {}", Rc::strong_count(&b));
-    println!("a rc count after changing a = {}", Rc::strong_count(&a));
-    
-    /*
-    这里出现了 a -> b -> a ....无限循环,stack overflow
-     */
-    // println!("{:?}",a.tail());
-}
-
-#[derive(Debug)]
-enum List{
-    Cons(i32,RefCell<Rc<List>>),
-    Nil,
-}
-
-impl List {
-    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
-        match self { 
-            Cons(_, item) => Some(item),
-            List::Nil => None,
+    let handle = thread::spawn(|| {
+        for i in 1..=10 {
+            println!("handle thread :{}", i);
+            thread::sleep(Duration::from_millis(1));
         }
-    }
+    });
+    thread::spawn(|| {
+       for i in 1..=5 {
+           println!("main thread :{}", i);
+           thread::sleep(Duration::from_millis(1));
+       } 
+    });
+    handle.join().unwrap();
 }
