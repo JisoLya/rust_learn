6d9b2e3328d8271e24c15ee837abf594d4ccf7bd
liuyan <SoLiuyan@163.com>
2025-02-13 22:28:59 +0800

    所有权和线程

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
 
 fn main() {
     /*
@@ -9,26 +8,15 @@ fn main() {
         mpsc::channel()表示multiple producer, single consumer
         返回一个元组，分别是发送端，接收端
      */
+    //可以使用mpsc::Sender::clone()来创建多个发送者
    let (tx,rx) = mpsc::channel();
     thread::spawn(move || {
-        thread::sleep(Duration::from_secs(2));
         let val = String::from("Hello World");
         tx.send(val).unwrap();
+        //所有权被移交
+        // println!("borrowed value {:?}",val);
     });
     
-    //会一直阻塞线程，直到收到消息
-    //try_recv 方法，不会阻塞，当获取到数据会返回Ok，里边包含着数据
-    //否则，返回错误
-    loop {
-        match rx.try_recv() { 
-            Ok(val) => {
-                println!("{}", val);
-                break;
-            },
-            Err(_) => {
-                println!("Waiting message...");
-                thread::sleep(Duration::from_secs(1));
-            },
-        };
-    }
+    let received = rx.recv().unwrap();
+    println!("{}", received); 
 }
