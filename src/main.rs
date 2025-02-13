d5abeda1b964be712b5eb88da5c7aebe200e6668
liuyan <SoLiuyan@163.com>
2025-02-13 22:12:24 +0800

    线程2 利用move将所有权移动到线程中

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,17 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
     /*
     rust 多线程,通过thread::spawn创建一个线程，接受一个闭包作为参数
-    
+
     并且可以使用join.Handle等待所有线程的完成
-    
+
     使用move闭包可以把一个线程的所有权转移到另一个线程当中
      */
+    let v = vec![1,2,3,4,5,6,7,8,9,10];
+    let handle = thread::spawn(move ||{
+        println!("{:?}", v);
+    });
     
-    let handle = thread::spawn(|| {
-        for i in 1..=10 {
-            println!("handle thread :{}", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-    thread::spawn(|| {
-       for i in 1..=5 {
-           println!("main thread :{}", i);
-           thread::sleep(Duration::from_millis(1));
-       } 
-    });
     handle.join().unwrap();
 }
