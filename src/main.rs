9da6cf9fd48dc28a3a34601d9f0f91221822c944
liuyan <SoLiuyan@163.com>
2025-02-13 22:23:57 +0800

    通信共享内存1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
+use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     /*
-    rust 多线程,通过thread::spawn创建一个线程，接受一个闭包作为参数
-
-    并且可以使用join.Handle等待所有线程的完成
-
-    使用move闭包可以把一个线程的所有权转移到另一个线程当中
+    通过消息来在线程之间传递信息
+        channel:包含发送端和接收端
+        mpsc::channel()表示multiple producer, single consumer
+        返回一个元组，分别是发送端，接收端
      */
-    let v = vec![1,2,3,4,5,6,7,8,9,10];
-    let handle = thread::spawn(move ||{
-        println!("{:?}", v);
+   let (tx,rx) = mpsc::channel();
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(2));
+        let val = String::from("Hello World");
+        tx.send(val).unwrap();
     });
     
-    handle.join().unwrap();
+    //会一直阻塞线程，直到收到消息
+    //try_recv 方法，不会阻塞，当获取到数据会返回Ok，里边包含着数据
+    //否则，返回错误
+    loop {
+        match rx.try_recv() { 
+            Ok(val) => {
+                println!("{}", val);
+                break;
+            },
+            Err(_) => {
+                println!("Waiting message...");
+                thread::sleep(Duration::from_secs(1));
+            },
+        };
+    }
 }
