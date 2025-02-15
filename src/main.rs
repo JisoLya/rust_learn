0f25a9bce256a83e9d67b7ca3b5043930bfcc2d5
liuyan <SoLiuyan@163.com>
2025-02-15 22:53:00 +0800

    模式匹配

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,137 @@
-use my_project::{Button, Draw, Screen};
-
 fn main() {
     /*
-       利用pub关键字可以实现封装
-       使用Trait对象来存储不同的类型 -> lib.rs
-       Trait对象执行的是动态派发
-       - 当Trait约束作用于泛型时，Rust编译器会执行单态化
-         - 编译器会为我们用来替换泛型类型参数的每一个具体类型生成对应的函数和方法的非泛型实现
-         - 通过单态化生成的代码会执行静态派发，在编译时确定运行方法
-         
-       - 当使用动态派发时，无法在编译过程中确定你调用的是哪一方法，在运行时会执行额外的代码来确定希望调用的方法
-       
-       Trait对象必须保证对象安全：
-       - 方法的返回值类型不是Self
-       - 方法中不包含任何泛型类型参数
-       //todo 解释一下对象安全
-       
-       状态模式
-       - 一个值拥有的内部状态由数个状态对象(state object)表达而成，而值的行为则随着内部状态的改变而改变
-       
-       这样在业务需求变化时，不需要修改持有值的代码，或者使用这个值的代码
-       只需要更新状态对象内部的代码，以便改变其规则，或者增加一些新的状态对象
+    模式匹配
+    if let
+    else if let
+    else...
+
+    while let ..
+     */
+    let mut vec1 = vec![1, 2, 3];
+    while let Some(numer) = vec1.pop() {
+        println!("{}", numer);
+    }
+
+    /*
+    匹配字面值
+     */
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("other"),
+    }
+
+    /*
+    匹配命名变量
+    命名的变量是可匹配任何值的无可辩驳模式
+     */
+    let x = Some(5);
+    let y = 10;
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched {:?}", y),
+        _ => println!("Default case: x = {:?}, y = {:?}", x, y),
+    }
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+
+    /*
+    使用 1|2匹配 1或者2
+     */
+    let x = 1;
+    match x {
+        1 | 2 => println!("one or two"),
+        _ => println!("other"),
+    }
+
+    /*
+    ..=匹配某个范围的值
     */
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("other"),
+    }
 
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 100,
-                options: vec![String::from("Soya"), String::from("Ros")],
-            }),
-            Box::new(Button {
-                width: 10,
-                height: 20,
-                label: String::from("Choice"),
-            }),
-        ],
-    };
+    /*
+    结构以分解值，可以使用模式来解构struct，enum，tuple
+    来使用不同部分
+     */
+    let p = Point { x: 23, y: 24 };
 
-    screen.run();
-}
+    // let Point { x: a, y: b } = p;
+    let Point { x, y } = p;
+    println!("x: {}, y: {}", x, y);
 
-struct SelectBox {
-    width: u32,
-    height: u32,
-    options: Vec<String>,
-}
-impl Draw for SelectBox {
-    fn draw(&self) {
-        //绘制
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    /*
+    可以用 _ 忽略值或者忽略值的某一部分
+     */
+    let s = Some(String::from("hello"));
+    //这样所有权也不会发生转移
+    if let Some(_) = s {
+        println!("found a string");
+    }
+    println!("{:?}", s);
+
+    /*
+    使用match守卫来提供额外的条件
+     */
+    let num = Some(4);
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+    /*
+    @ 符号让我们创建一个变量，该变量可以在测试某个值是否匹配模式的同时保存该值
+     */
+    let msg = Message::Hello { id: 5 };
+    let msy = Message::Tome(5);
+    match msg{
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => {
+            println!("Found an id in range: {}", id_variable)
+        }
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => {
+            println!("Found some other id: {}", id)
+        }
+        Message::Tome(y) => println!("y in Tome is {}",y),
+    }
+
+    match msy{
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => {
+            println!("Found an id in range: {}", id_variable)
+        }
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => {
+            println!("Found some other id: {}", id)
+        }
+        Message::Tome(y) => println!("y in Tome is {}",y),
     }
 }
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+enum Message {
+    Hello { id: i32 },
+    Tome(i32),
+}
